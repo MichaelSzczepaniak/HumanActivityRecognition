d017c83ff7caff1edb4ac348869961e9e26151d8
MichaelSzczepaniak <mszczepaniak@pobox.com>
2015-07-22 19:29:20 -0600

    remove data dir rename to workaround concurrency issue

--- run_analysis.R
+++ run_analysis.R
@@ -3,10 +3,12 @@ library(downloader)
 
 ## 
 ## 
-run_analysis <- function(options = "fromScratch") {
+run_analysis <- function(options = "fromDeparsedData") {
     xtestdata <- NULL
     xtraindata <- NULL
     if(options == "fromScratch"){
+        cat(format(Sys.time(), "%T"),
+            "HAR data download started...\n")
         getHARData()  # gets the zipped data file, explodes it, and renames dir
         cat(format(Sys.time(), "%T"), "HAR data download complete...\n")
         createDeparsedXData()  # creates the output directory then
@@ -15,13 +17,15 @@ run_analysis <- function(options = "fromScratch") {
         cat(format(Sys.time(), "%T"),
             "Deparsed HAR X data file creations complete...\n")
     } else if(options == "fromDeparsedData") {
-        
+        xtestdata <- readDeparsedXData(TRUE)
+        xtestdata <- readDeparsedXData(FALSE)
     } else {
         cat("Invalid option. Valid options are:\n")
         cat("fromScratch or fromDeparsedData\n")
         cat("See README.md for details.\n")
         cat("No computation done. Exiting.")
     }
+    
 }
 
 ## Downloads the Human Activity Recognition data, unzips it, renames the
@@ -30,25 +34,29 @@ getHARData <- function (datafile = "data.zip") {
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     download(fileUrl, destfile=paste0("./", datafile))
     unzip(datafile)
-    file.rename("UCI HAR Dataset", "data")
-    file.remove(datafile)
+    #file.rename("UCI HAR Dataset", "data")
+    #cat(format(Sys.time(), "%T"),
+    #    "'UCI HAR Dataset' dir renamed to 'data'...\n")
     loadDirectoryStructure()
+    file.remove(datafile)
 }
 
 ## Simply loads some variables that define the directory structure of the
 ## dataset into the workspace so that it can be retrieved later by other
 ## operations.
 loadDirectoryStructure <- function() {
-    dataDir <<-  "./data"
+    dataDir <<-  "./UCI HAR Dataset"
     testDir <<- paste0(dataDir, "/test")
     trainDir <<- paste0(dataDir, "/train")
     outputDir <<- paste0(dataDir, "/output")
 }
 
-##
-##
+## Reads the raw HAR data files into dataframes and then deparses them into
+## the following locations: ../data/output/xtestdf.R and
+##                          ../data/output/xtraindf.R
+## These objects are created to speed future reconstruction of these dataframes.
 createDeparsedXData <- function() {
-    dir.create("./data/output")
+    dir.create(outputDir)
     xtestdata <- readXFromRawData(TRUE)
     cat(format(Sys.time(), "%T"),
                " Finished reading X_test.txt data file into data frame.\n")
@@ -63,7 +71,7 @@ createDeparsedXData <- function() {
         " Finished dput of xtestdf.R.\n")
 }
 
-## Reads the data from a raw HAR data file into a dataframe.
+## Reads the data from a raw HAR data file and returns a dataframe.
 ## testData - TRUE if file to be read is the X_test.txt file
 ##            FALSE if the file to be read is the X_train.txt file
 readXFromRawData <- function(testData) {
@@ -78,6 +86,25 @@ readXFromRawData <- function(testData) {
     # 561 factors (variables). Each value is 15 characters seperated by
     # a space.
     xdata <- read.fwf(file = rawDataFilePath, rep(c(-1, 15), 561))
+    
+    return(xdata)
+}
+
+## Reads the data from a deparsed HAR dataframe and returns the reconstructed
+## dataframe.  These files are created from createDeparsedXData function:
+##     xtestdf.R (created from X_test.txt) and
+##     xtraindf.R (crreated from X_train.txt)
+## Note: This function is much faster than readXFromRawData which reads the
+##       raw HAR data file.
+## testData - TRUE if file to be read is the xtestdf.R file
+##            FALSE if the file to be read is the xtraindf.R file
+readDeparsedXData <- function(testData) {
+    xdata <- NULL
+    if(testData) {
+        xdata <- dget(paste0(testDir, "xtestdf.R"))
+    } else {
+        xdata <- dget(paste0(testDir, "xtraindf.R"))
+    }
     
     return(xdata)
 }
